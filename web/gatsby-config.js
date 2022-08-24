5a05e605cba1be6ca403775cd374feaa03108808
Zeeohn <successzion21@gmail.com>
2022-08-24 11:41:33 +0100

    dark mode plugin

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -3,8 +3,8 @@ require('dotenv').config('./.env');
 module.exports = {
   siteMetadata: {
     title: `HealthGuru-blog`,
-    siteUrl: `https://techhub-blog.netlify.com`,
+    siteUrl: ``,
     description: `Health Guru blog is a blog that advises on health related issues, physical and mental wellness and best health practices.`,
   },
-  plugins: [],
+  plugins: ['gatsby-plugin-dark-mode'],
 };
